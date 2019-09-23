d4413b9afa623dced3f6d4a68c70489545c6a94d
camendola <chia.amendola@gmail.com>
2019-09-23 13:36:31 +0200

    first commit

---
+++ scripts/train.sh
@@ -0,0 +1,5 @@
+#tautau BDT
+python training/VBFvsGGF.py --cv_BDT --show --selection "type"
+python training/VBFvsGGF.py --cv_BDT --show --selection "type-baseline"
+python training/VBFvsGGF.py --cv_BDT --show --selection "type-baseline-trigger"
+
